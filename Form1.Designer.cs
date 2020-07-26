dc96d103dcedbf55b026a7cbee206e39d373fd00
ivagcu <40316284+ivagcu@users.noreply.github.com>
2020-07-26 21:29:15 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PoolWizard
+{
+    partial class PoolWizardX
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PoolWizardX));
+            this.InfoUP = new System.Windows.Forms.Label();
+            this.WalletLabel = new System.Windows.Forms.Label();
+            this.textBoxWallet = new System.Windows.Forms.TextBox();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.UsuarioLabel = new System.Windows.Forms.Label();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InfoUP
+            // 
+            this.InfoUP.AutoSize = true;
+            this.InfoUP.BackColor = System.Drawing.Color.Transparent;
+            this.InfoUP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InfoUP.Location = new System.Drawing.Point(25, 102);
+            this.InfoUP.Name = "InfoUP";
+            this.InfoUP.Size = new System.Drawing.Size(286, 20);
+            this.InfoUP.TabIndex = 0;
+            this.InfoUP.Text = "Por favor, rellene las casillas y confirme:";
+            this.InfoUP.Click += new System.EventHandler(this.InfoUP_Click);
+            // 
+            // WalletLabel
+            // 
+            this.WalletLabel.AutoSize = true;
+            this.WalletLabel.BackColor = System.Drawing.Color.Transparent;
+            this.WalletLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WalletLabel.Location = new System.Drawing.Point(25, 162);
+            this.WalletLabel.Name = "WalletLabel";
+            this.WalletLabel.Size = new System.Drawing.Size(145, 20);
+            this.WalletLabel.TabIndex = 1;
+            this.WalletLabel.Text = "Cartera MONERO: ";
+            // 
+            // textBoxWallet
+            // 
+            this.textBoxWallet.Location = new System.Drawing.Point(189, 164);
+            this.textBoxWallet.Name = "textBoxWallet";
+            this.textBoxWallet.Size = new System.Drawing.Size(159, 20);
+            this.textBoxWallet.TabIndex = 2;
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Location = new System.Drawing.Point(189, 215);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(159, 20);
+            this.textBoxUsuario.TabIndex = 4;
+            // 
+            // UsuarioLabel
+            // 
+            this.UsuarioLabel.AutoSize = true;
+            this.UsuarioLabel.BackColor = System.Drawing.Color.Transparent;
+            this.UsuarioLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsuarioLabel.Location = new System.Drawing.Point(25, 215);
+            this.UsuarioLabel.Name = "UsuarioLabel";
+            this.UsuarioLabel.Size = new System.Drawing.Size(135, 20);
+            this.UsuarioLabel.TabIndex = 3;
+            this.UsuarioLabel.Text = "Usuario (EMAIL): ";
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInfo.Location = new System.Drawing.Point(75, 290);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(100, 50);
+            this.btnInfo.TabIndex = 5;
+            this.btnInfo.Text = "Info";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmar.Location = new System.Drawing.Point(225, 290);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(100, 50);
+            this.btnConfirmar.TabIndex = 6;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(101, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "PoolWizardX";
+            // 
+            // PoolWizardX
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Controls.Add(this.UsuarioLabel);
+            this.Controls.Add(this.textBoxWallet);
+            this.Controls.Add(this.WalletLabel);
+            this.Controls.Add(this.InfoUP);
+            this.Name = "PoolWizardX";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PoolWizardX_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Label InfoUP;
+        private System.Windows.Forms.Label WalletLabel;
+        private System.Windows.Forms.TextBox textBoxWallet;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.Label UsuarioLabel;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Windows.Forms;
+
+namespace PoolWizard
+{
+    public partial class PoolWizardX : Form
+    {
+        public PoolWizardX()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+        }
+
+        private void InfoUP_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Comprobar los navegadores que tiene instalados el usuario
+                RegistryKey browserKeys;
+                //on 64bit the browsers are in a different location
+                browserKeys = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Clients\StartMenuInternet");
+                if (browserKeys == null)
+                    browserKeys = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Clients\StartMenuInternet");
+
+                string[] browserNames = browserKeys.GetSubKeyNames();
+                var psi = new System.Diagnostics.ProcessStartInfo("iexplore.exe");
+
+                foreach (String s in browserNames)
+                {
+                    if (s.Contains("Chrome"))
+                    psi = new System.Diagnostics.ProcessStartInfo("chrome.exe");
+                }
+
+                psi.Arguments = "http://www.google.com/";
+                System.Diagnostics.Process.Start(psi);
+            }
+            catch (System.ComponentModel.Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                    MessageBox.Show(noBrowser.Message);
+            }
+            catch (System.Exception other)
+            {
+                MessageBox.Show(other.Message);
+            }
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            if ((textBoxUsuario.TextLength > 5) && (textBoxWallet.TextLength > 20) && (textBoxUsuario.Text.Contains("@")))
+            {
+
+                //SACAR NOMBRE DE USUARIO DEL EQUIPO
+                string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+                string[] tokens = userName.Split('\\');
+                userName = tokens[1];
+
+                //CREAR FICHERO
+                //OBTENER DATOS DE TEXTBOXES
+                string pool = "xmrpool.eu:9999";
+                string wallet = textBoxWallet.Text;
+                string user = textBoxUsuario.Text;
+
+                //GENERAR STRING CON EL CONTENIDO DEL BAT
+                string batLoc = "xmrig.exe --donate-level 1 --max-cpu-usage 50 --cpu-priority 0 -o " + pool + " -u " + wallet + " -k --tls --rig-id " + user;
+                string batIni = @"C:\Users\" + userName + @"\Desktop\Dollars4Free\xmrig.exe --donate-level 1 --max-cpu-usage 50 --cpu-priority 0 -o " + pool + " -u " + wallet + " -k --tls --rig-id " + user;
+
+                // Folder, where a file is created.  
+                // Make sure to change this folder to your own folder  
+                string folder = @"C:\Users\" + userName + @"\Desktop\Minado\Monero\";
+                // Filename  
+                string fileName = "exe.bat";
+                // Fullpath. You can direct hardcode it if you like.  
+                string fullPathLoc = folder + fileName;
+                string fullPathIni = @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\exe.bat";
+                // If the file does not exists, it will create a new file.  
+                // This method automatically opens the file, writes to it, and closes file  
+                //AÑADIR FICHEROS BAT A CARPETA LOCAL Y A INICIO
+                File.WriteAllText(fullPathLoc, batLoc);
+                File.WriteAllText(fullPathIni, batIni);
+
+                //ARRANCAR PROCESO DE MINADO
+                string strCmdText = "/C cd " + folder + "&& " + fileName;
+                ProcessStartInfo pStartInfo = new ProcessStartInfo("CMD.exe", strCmdText);
+                pStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                Process p = new Process();
+                p.StartInfo = pStartInfo;
+                p.Start();
+
+            }
+            else if (textBoxWallet.TextLength != 95)
+            {
+                MessageBox.Show("Introduce una cartera de Monero válida de 95 caracteres.");
+            }
+            else if ((textBoxUsuario.TextLength < 10) || (!textBoxUsuario.Text.Contains("@")))
+            {
+                MessageBox.Show("Introduce un correo válido.");
+            }
+        }
+        
+        private void PoolWizardX_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            //ACCIONES A REALIZAR AL CERRAR LA VENTANA
+            bool minando = false;
+            Process[] mineros;
+            mineros = Process.GetProcessesByName("xmrig");
+            
+            //COMPROBAR SI SE ESTÁ MINANDO
+            foreach(Process proc in mineros)
+            {
+                minando = true;
+            }
+
+            //SI SE ESTÁ MINANDO, CERRAR PROGRAMA DE MINADO
+            if(minando == true)
+            {
+                System.Diagnostics.Process.Start("cmd.exe", "/c taskkill /F /IM xmrig.exe /T");
+            }
+
+        }
+    }
+}
+
+    
+
+
